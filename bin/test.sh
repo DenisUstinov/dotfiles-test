372d471c5aa573a08f03c3f23d2a4b6d019fe661
Denis Ustinov <83418606+DenisUstinov@users.noreply.github.com>
2025-12-21 06:57:46 +0700

    Update test.sh

--- bin/test.sh
+++ bin/test.sh
@@ -74,8 +74,8 @@ TARGET_SSH_DIR_PERMS=700
 TARGET_AUTH_PERMS=600
 TARGET_SSH_KEY=""
 while [[ -z "$TARGET_SSH_KEY" ]]; do
-    OLD_HISTFILE="${HISTFILE:-}"
-    OLD_HISTSIZE="${HISTSIZE:-}"
+    OLD_HISTFILE="${HISTFILE-}"
+    OLD_HISTSIZE="${HISTSIZE-}"
     unset HISTFILE
     HISTSIZE=0
     read -rsp "Enter GitHub Personal Access Token: " GH_TOKEN
