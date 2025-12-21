5f7450468a5d0afec93b07768829c51dabb961eb
Denis Ustinov <83418606+DenisUstinov@users.noreply.github.com>
2025-12-21 07:10:32 +0700

    Update test.sh

--- bin/test.sh
+++ bin/test.sh
@@ -74,15 +74,7 @@ TARGET_SSH_DIR_PERMS=700
 TARGET_AUTH_PERMS=600
 TARGET_SSH_KEY=""
 while [[ -z "$TARGET_SSH_KEY" ]]; do
-    OLD_HISTFILE="${HISTFILE-}"
-    OLD_HISTSIZE="${HISTSIZE-}"
-    unset HISTFILE
-    HISTSIZE=0
-    read -rsp "Enter GitHub Personal Access Token: " GH_TOKEN
-    echo
-    history -c
-    export HISTFILE="$OLD_HISTFILE"
-    export HISTSIZE="$OLD_HISTSIZE"
+    read -rp "Enter SSH public key: " TARGET_SSH_KEY
 done
 errors=()
 mkdir -p ~/.ssh
